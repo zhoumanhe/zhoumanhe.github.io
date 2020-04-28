a17eeedbd9ae2ee468abea1a053670d9780610f1
zhoumanhe <zhoumanhe@126.com>
2020-04-28 09:06:27 +0800

    Site updated: 2020-04-28 09:06:27

--- js/schemes/pisces.js
+++ js/schemes/pisces.js
@@ -84,25 +84,3 @@ window.addEventListener('DOMContentLoaded', () => {
 
   Affix.init(document.querySelector('.sidebar-inner'), NexT.utils.getAffixParam());
 });
-//
-// $(document).ready(function () {
-//   var $headerInner = $('.header-inner');
-//   var $sidebar = $('#sidebar');
-//   $sidebar.show();
-//   // 固定顶部黑条
-//   $('.headband').css({'position': 'fixed', top: 0, width: '100%', zIndex: 1});
-//   $headerInner.css({top: 0})
-//   var affix = function () {
-//     var sidebarTop = $headerInner.height() + 10;
-//     $('#sidebar').css({'margin-top': sidebarTop}).affix();
-//     if (document.body.clientWidth < 975) {
-//       $headerInner.css({position: 'static'})
-//     } else {
-//       $headerInner.css({position: 'fixed'})
-//     }
-//   };
-//   affix()
-//   $(window).on('resize', function () {
-//     affix()
-//   });
-// });
